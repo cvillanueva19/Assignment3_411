fc178e8378431b4dba02ce4d67fc69ad1d5ba0ca
Chris Villanueva <cvillanueva19@csu.fullerton.edu>
2017-03-19 20:16:28 -0700

    Initial Commit

---
+++ Practice MVC 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice MVC 2
+//
+//  Created by CampusUser on 3/19/17.
+//  Copyright © 2017 Fullerton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
